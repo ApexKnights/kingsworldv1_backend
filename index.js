4627a70f521cd2ac08e6990e1133b63a0b72c167
Souparno <souparna2000shee@gmail.com>
2024-12-30 19:36:47 +0530

    change

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://kingsworldofficial.in/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }))
